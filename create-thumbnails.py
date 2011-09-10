2890450232a02ef06ed7e7dd93599215800fe253
Philipp Matthias Hahn <pmhahn@pmhahn.de>
2011-09-10 08:12:12 +0200

    Create thumbnail-factory only one
    Move the creation of the thumbnail factory out of the loop to create it
    only once at the beginning.

--- create-thumbnails.py
+++ create-thumbnails.py
@@ -28,12 +28,12 @@ import sys
 
 dir=sys.argv[1]
 
+thumbFactory = gnome.ui.ThumbnailFactory(gnome.ui.THUMBNAIL_SIZE_LARGE)
 for subdir, dirs, files in os.walk(dir):
  for file in files:
   uri=gnomevfs.get_uri_from_local_path(subdir+"/"+file)  
   mime=gnomevfs.get_mime_type(subdir+"/"+file)
   mtime=int(time.strftime("%s",time.localtime(os.path.getmtime(subdir+"/"+file))))
-  thumbFactory = gnome.ui.ThumbnailFactory(gnome.ui.THUMBNAIL_SIZE_LARGE)
   if not os.path.exists(gnome.ui.thumbnail_path_for_uri(uri, gnome.ui.THUMBNAIL_SIZE_LARGE)) and thumbFactory.can_thumbnail(uri ,mime, 0):
       print "Generating for "+uri
       thumbnail=thumbFactory.generate_thumbnail(uri, mime)
